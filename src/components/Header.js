f2f535660458bee33638523ef19677186b262544
jayathracm <jayathracm@gmail.com>
2024-11-20 11:11:18 +0200

    Added favicon and navigation menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,53 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import './Header.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faRightToBracket, faUser  } from '@fortawesome/free-solid-svg-icons';
-import { useNavigate } from 'react-router-dom'; // Import useNavigate
+import { faRightToBracket, faUser , faBars } from '@fortawesome/free-solid-svg-icons'; // Import the bars icon
+import { useNavigate } from 'react-router-dom';
 
 function Header() {
-  const navigate = useNavigate(); // Initialize useNavigate
+  const [menuVisible, setMenuVisible] = useState(false); // State to manage menu visibility
+  const menuRef = useRef(null); // Reference for the menu
+  const navigate = useNavigate();
+
+  const toggleMenu = () => {
+    setMenuVisible(!menuVisible); 
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setMenuVisible(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
 
   return (
     <header className="header">
-      <h1 className="title" onClick={() => navigate('/')}>ReelRadar</h1> {/* Add onClick handler */}
+      <div className="header-content">
+        <button className="menu-button" onClick={toggleMenu}>
+          <FontAwesomeIcon icon={faBars} />
+        </button>
+        <h1 className="title" onClick={() => navigate('/')}>ReelRadar</h1>
+      </div>
       <div className="icons">
         <span className="icon"><FontAwesomeIcon icon={faRightToBracket} /></span>
-        <span className="icon"><FontAwesomeIcon icon={faUser } /></span>
+        <span className="icon"><FontAwesomeIcon icon={faUser  } /></span>
       </div>
+      {menuVisible && (
+        <div className="menu" ref={menuRef}>
+          <ul>
+            <li onClick={() => navigate('/path1')}>Genres</li>
+            <li onClick={() => navigate('/path2')}>Filter by Year</li>
+            <li onClick={() => navigate('/path2')}>Filter by Rating</li>
+            <li onClick={() => navigate('/path2')}>Groups</li>
+          </ul>
+        </div>
+      )}
     </header>
   );
 }
