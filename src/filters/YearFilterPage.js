3488e634f55c44bb3ef2fde8a267f715cafd74fc
Anil <t3shan00@students.oamk.fi>
2024-12-07 18:37:23 +0200

    1

--- src/filters/YearFilterPage.js
+++ src/filters/YearFilterPage.js
@@ -14,7 +14,7 @@ const YearFilterPage = () => {
   const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
 
-  const API_KEY = "6e9e4df1f8d6a6a540ccf27bb6efc253"; //added api instead of .env
+  const API_KEY = "6e9e4df1f8d6a6a540ccf27bb6efc253"; 
 
   useEffect(() => {
     const currentYear = new Date().getFullYear();
